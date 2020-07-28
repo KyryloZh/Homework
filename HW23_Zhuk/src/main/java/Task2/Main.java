a7ee021c8d832a75d2fe0f717259438ad22e4daa
Kyrylo Zh <incountable@gmail.com>
2020-07-28 17:12:15 +0300

    completed 4 out of 8 cases

--- HW23_Zhuk/src/main/java/Task2/Main.java
+++ HW23_Zhuk/src/main/java/Task2/Main.java
@@ -7,21 +7,68 @@ public class Main {
         ZooClub zooClub = new ZooClub();
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welcome to our ZooClub. Please choose one of the options below: ");
-        System.out.println("1. Add new club member." + "\n" + "2. Add club member's pet." + "\n"
-                + "3. Delete club member's pet." + "\n" + "4. Delete club member." + "\n"
+        System.out.println("Welcome to our ZooClub.");
+
+        inputloop : while (true) {
+        System.out.print("Please choose one of the options below:" + '\n'
+                + "1. Add new club member." +           "\n" + "2. Add club member's pet." + "\n"
+                + "3. Delete club member's pet." +         "\n" + "4. Delete club member." + "\n"
                 + "5. Delete a pet from all club members." + "\n" + "6. Show all records." + "\n"
                 + "7. Exit" + "\n" + "Make your choice: ");
 
-        int choice = sc.nextInt();
 
-        switch (choice){
-            case 1:{
-                System.out.println("Enter member's name: ");
-                String name = sc.next();
-                System.out.println("Enter member's age: ");
-                int age = sc.nextInt();
-                zooClub.addPerson(name, age);
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1: {
+                    System.out.print("So you want to add a new club member. What's his name? ");
+                    String name = sc.next();
+                    System.out.print("And his age: ");
+                    int age = sc.nextInt();
+                    zooClub.addPerson(name, age);
+                    System.out.println("Welcome, " + name + "!");
+                    break;
+                }
+                case 2: {
+                    System.out.print("Let's add a new pet! First, what's the type? ");
+                    String type = sc.next();
+                    System.out.print("Name of the pet as well, please: ");
+                    String name = sc.next();
+                    System.out.print("Whose pet is it?Please enter owner's name:" );
+                    String ownersName = sc.next();
+                    zooClub.addAnimal(type, name, ownersName);
+
+                    break;
+                }
+                case 3: {
+                    //not a duplicate branch
+                    break;
+                }
+                case 4: {
+                    System.out.print("Who do you want to delete? Please enter a name. ");
+                    String name = sc.next();
+                    zooClub.removePerson(name);
+                    System.out.println(name + " and his pet were deleted from the club list.");
+                }
+                case 5: {
+
+                    break;
+                }
+                case 6: {
+                    zooClub.ShowAll();
+                    break;
+                }
+                case 7: {
+                    System.out.println("Bye!");
+                    break inputloop;
+                }
+                default: {
+                    System.out.println("Please pick an option from the offered ones.");
+
+                    break;
+
+                }
+
+
             }
         }
 
--- HW23_Zhuk/src/main/java/Task2/ZooClub.java
+++ HW23_Zhuk/src/main/java/Task2/ZooClub.java
@@ -1,15 +1,32 @@
 package Task2;
 
-import java.util.List;
-
-import java.util.Map;
+import java.util.*;
 
 public class ZooClub {
-    private Map<Person, List<Animal>>map;
+    private Map<Person, List<Animal>> map = new HashMap<Person, List<Animal>>();
 
-    public void addPerson (String name, int age){
-        Person person = new Person (name, age);
-        System.out.println(person);
+    public void addPerson(String name, int age) {
+        Person person = new Person(name, age);
+        map.put(person, null);
+    }
 
+    public void removePerson(String name) {
+        map.remove(name);
+    }
+
+
+    public void addAnimal(String type, String name, String ownersName) {
+        Animal animal = new Animal(type, name);
+        List<Animal> animalsList = new LinkedList<Animal>();
+        animalsList.add(animal);
+    }
+
+
+    public void ShowAll() {
+        for(Map.Entry<Person, List<Animal>> pair : map.entrySet()){
+            if (pair.getValue() == null) {
+                System.out.println("Our club members are:" + '\n' + pair.getKey() + " - " + "No animal records.");
+            } else { System.out.println("Our club members are:" + '\n' + pair.getKey() + " - " + pair.getValue()); }
+        }
     }
 }
